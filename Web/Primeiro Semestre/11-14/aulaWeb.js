76d3c1c2f15f340de96e7ba739f74fa8c94f8c6e
diogo <alvarwngadiogo228@gmail.com>
2025-11-14 16:03:54 -0300

    commit

---
+++ Web/Primeiro Semestre/11-14/aulaWeb.js
@@ -0,0 +1,56 @@
+
+let nome = "Marcos";
+let idade = 25;
+let aprovado = true;
+let endereco = null;
+let telefone;
+
+console.log(nome,idade,aprovado,endereco,telefone);
+
+let a = 45;
+let b = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+
+
+//==================================
+
+
+let nome = "Diogo";
+let sobrenome = "Alvarenga";
+let idade = 21;
+let curso = "DSM";
+let salario = 1.000;
+let cidade = "Franco da Rocha";
+const sexo = "M";
+let casado = false;
+
+console.log(nome,sobrenome,idade,curso,salario,cidade,sexo,casado);
+
+
+//=====================================
+
+const pessoa ={
+    nome: "Jose",
+    sobrenome: "Themoteo",
+    idade:45,
+    curso: "Engenharia da Computação",
+    salario: 4500.00,
+    cidade: "São Paulo",
+    sexo: "Masculino",
+    casado: true 
+}
+
+let nom1 = "Themoteo";
+
+console.log("Nome:",nom1);
+console.log("------------------------------------");
+
+console.log("Nome:",pessoa.nome);
+console.log("Sobrenome:",pessoa.sobrenome);
+console.log("Idade:",pessoa.idade);
+console.log("Curso:",pessoa.curso);
+console.log("Salario: R$",pessoa.salario);
+console.log("Sexo:",pessoa.sexo);
+console.log("Casado:",pessoa.casado);
